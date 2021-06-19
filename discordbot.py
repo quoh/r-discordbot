3ef3ae32f2c5b957795a32d003809ab4f8e7dcec
shipcat <tag1997k@gmail.com>
2021-06-20 02:15:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -51,45 +51,6 @@ async def on_ready():
     login = f'{now} :起動したわ！'
     await channel.send(login)
 
-# @client.event
-# async def on_disconnect():
-#     # 起動したらターミナルにログイン通知が表示される
-#     print('ログアウトしました')
-#     now = datetime.now(JST).strftime("%Y/%m/%d %H:%M:%S")
-#     channel = client.get_channel(channelID)
-#     login = f'{now} :シャットダウンするわ！'
-#     await channel.send(login)
-
-@client.event
-async def on_voice_state_update(member, before, after):
-  global nowConnect
-  df_vclog = pd.read_csv('vclog.csv')
-  if member.guild.id == 0000000000000000000:
-        # print("channel accepted!")
-        now = datetime.now(JST).strftime("%Y/%m/%d %H:%M:%S")
-        alert_channel = client.get_channel(0000000000000000000)
-        if before.channel is None: 
-            msg = f'{now} に {member.name} が {after.channel.name} に参加しました。'
-            await alert_channel.send(msg)
-            nowConnect.append([member.name,after.channel.name , datetime.now(JST)])
-            print(nowConnect)
-        elif after.channel is None: 
-            msg = f'{now} に {member.name} が {before.channel.name} から退出しました。'
-            await alert_channel.send(msg)
-            arr = np.array(nowConnect)
-            pos = str(tuple(nd[0] for nd in np.where(arr == member.name))) #posは(0,0)みたいに帰ってくるので、無理やり引っ張る　賢いやり方あるんだろうけど一時的に
-            possess = int((pos.split(',')[0])[1:])
-            #csvに時間を保存
-            time = nowConnect[possess][2]
-            times = datetime.now(JST) - time
-            insert = {'name': member.name, 'channel': before.channel.name, 'time': times}
-            df_vclog_ins = df_vclog.append(insert, ignore_index=True)
-            df_vclog_ins.to_csv('vclog.csv', index=None)
-            #配列の中からユーザ名を見つけてきて、それを消す処理
-            print(df_vclog)
-            nowConnect.pop(possess)
-            print(nowConnect)
-
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
@@ -98,11 +59,13 @@ async def on_message(message):
     # global bot
     word = message.content
     now = datetime.now(JST).strftime("%Y/%m/%d %H:%M:%S")
+    # messageの送信主がBotならスルー
     if message.author.bot:
         return
+    # messageが特定のチャンネルで画像が送信されたらそれに対してランダムでリアクションをつける
     if message.attachments:
         isReact = False
-        print(message.channel)
+        # print(message.channel)
         if str(message.channel) == "botstatus":
             for attachment in message.attachments:
                 # Attachmentの拡張子がpng, jpg, jpegのどれかだった 場合
@@ -111,24 +74,22 @@ async def on_message(message):
             if isReact == True:
                 await message.add_reaction(random.choice(emojiList))
     # 「/neko」と発言したら「にゃーん」が返る処理
-    if word == '$neko':
+    if word == '/neko':
         await message.channel.send('にゃーん')
     # ヘルプの表示
-    elif word == '$ghelp':
+    elif word == '/ghelp':
         await message.channel.send(help)
     #チャンネルの作成
-    elif word.startswith('$mkch'):
+    elif word.startswith('/mkch'):
       mkch = word.split(' ')
       new_channel = await create_channel(message, channel_name=mkch[1])
       channel = client.get_channel(channelID)
       # チャンネルのリンクと作成メッセージを送信
       text = f'{now} に {message.author.mention} が {new_channel.mention} を作成したわ！'
       await channel.send(text)
-    elif word.startswith('$join'):
+    elif word.startswith('/join'):
       voich = await discord.VoiceChannel.connect(message.author.voice.channel)
-    elif word.startswith('$disconnect'):
-      await voich.disconnect()
-    elif word.startswith('$vchoice'):
+    elif word.startswith('/vchoice'):
       vchoi = word.split(' ')
       count = 1
       if len(vchoi) == 2:
@@ -150,7 +111,7 @@ async def on_message(message):
         text = 'エラーよ！ちゃんと!ghelpみて確認しなさい！'
       await message.channel.send(text)
     # !dice n m 'sum'
-    elif word.startswith('$dice'):
+    elif word.startswith('/dice'):
       roll = word.split(' ')
       sum = 0
       isSum = False
@@ -158,19 +119,15 @@ async def on_message(message):
         isSum = True
       men = int(roll[1])
       count = int(roll[2])
-      rtn = []
-      for i in range(count):
-        rollrnd = random.randint(1,men)
-        rtn.append(rollrnd)
-        if isSum == True:
-          sum = sum + rollrnd
-      text = f'{men}面ダイスを{count}回回したわ！\n結果は、\n{rtn}よ！'
-      textSum = f'合計は、{sum}よ！'
+      rtnDice =  [random.randint(1,men) for i in range(count)]
+      rtnDiceSum = sum(rtnDice)
+      text = f'{men}面ダイスを{count}回回したわ！\n結果は、\n{rtnDice}よ！'
+      textSum = f'合計は、{rtnDiceSum}よ！'
       await message.channel.send(text)
       if isSum == True:
         await message.channel.send(textSum)
     # 課題曲を提示してくれる
-    elif word.startswith('$sdvxk'):
+    elif word.startswith('/sdvxk'):
       chl = client.get_channel(channelID)
       sdvx = word.split(' ')
       difficulty = sdvx[1]
@@ -178,6 +135,7 @@ async def on_message(message):
       if len(sdvx) == 3:
         cnt = int(sdvx[2])
       df_query = dfs.query('difficulty == ' + difficulty)
+      # 重複有でcsvから抽出
       rtndf = df_query.sample(n=cnt,replace=True)
       rtntext = ''
       for item, rows in rtndf.iterrows():
@@ -185,15 +143,6 @@ async def on_message(message):
       # print(rtntext)
       text = f'Lv. {difficulty}の曲を{cnt}個選んだわ！\n\n{rtntext}\nちゃんとやりなさいよ！'
       await chl.send(text)
-    #指定した範囲で課題曲を選択する
-    elif word.startswith('$sdvxr'):
-      chl = client.get_channel(channelID)
-      sdvx = word.split(' ')
-      difficulty_l = sdvx[1]
-      difficulty_m = sdvx[2]
-      cnt = 3
-      if len(sdvx) == 4:
-        cnt = int(sdvx[3])
     # 話かけられたとき
     if client.user in message.mentions: # 話しかけられたかの判定
         # オウム返し
@@ -207,6 +156,7 @@ async def reply(message):
     reply = f'{message.author.mention} 💩を投げとくわね' # 返信メッセージの作成
     await message.channel.send(reply) # 返信メッセージを送信
 
+#チャンネルの作成ができる関数　おまじない
 async def create_channel(message, channel_name):
     category_id = message.channel.category_id
     category = message.guild.get_channel(category_id)
